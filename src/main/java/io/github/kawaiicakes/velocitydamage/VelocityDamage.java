b7593e5f971595669c9d6511a8fdc71200ebfa86
kawaiicakes <vomitmopper@gmail.com>
2023-10-18 22:57:31 -0600

    playing around (also need to access this from laptop)

--- src/main/java/io/github/kawaiicakes/velocitydamage/VelocityDamage.java
+++ src/main/java/io/github/kawaiicakes/velocitydamage/VelocityDamage.java
@@ -1,8 +1,14 @@
 package io.github.kawaiicakes.velocitydamage;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -12,9 +18,17 @@ public class VelocityDamage
 {
     public static final String MOD_ID = "velocitydamage";
     private static final Logger LOGGER = LogUtils.getLogger();
+
     public VelocityDamage()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(VelocityDamage.class);
+    }
+
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event) {
+        if (!(event.getSource().getEntity() instanceof ServerPlayer player)) return;
+
+        // 12 seconds
+        event.setAmount(event.getAmount() * 400_000F);
     }
 }
