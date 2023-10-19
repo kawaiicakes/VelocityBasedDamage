c9fd6196ee463eebe11eab1c392bab0cad90cf14
kawaiicakes <vomitmopper@gmail.com>
2023-10-19 03:01:44 -0600

    changes for debugging in-game

--- src/main/java/io/github/kawaiicakes/velocitydamage/VelocityDamage.java
+++ src/main/java/io/github/kawaiicakes/velocitydamage/VelocityDamage.java
@@ -5,6 +5,7 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,6 +13,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.util.Objects;
 
 @Mod(VelocityDamage.MOD_ID)
 public class VelocityDamage
@@ -26,9 +29,11 @@ public class VelocityDamage
 
     @SubscribeEvent
     public static void onLivingHurt(LivingHurtEvent event) {
+        LOGGER.debug("Attack pre-change: " + event.getAmount());
         if (!(event.getSource().getEntity() instanceof ServerPlayer player)) return;
-
+        if (!(player.getMainHandItem().getItem() instanceof SwordItem)) return;
         // 12 seconds
         event.setAmount(event.getAmount() * 400_000F);
+        LOGGER.debug("Attack post-change: " + event.getAmount() * 400_000F);
     }
 }
