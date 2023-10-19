5247fb1b8c25eb4f36728c494ce0f559de7918ca
kawaiicakes <vomitmopper@gmail.com>
2023-10-19 04:01:25 -0600

    naive functionality

--- src/main/java/io/github/kawaiicakes/velocitydamage/VelocityDamage.java
+++ src/main/java/io/github/kawaiicakes/velocitydamage/VelocityDamage.java
@@ -1,20 +1,14 @@
 package io.github.kawaiicakes.velocitydamage;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.FishingRodItem;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.Objects;
 
 @Mod(VelocityDamage.MOD_ID)
 public class VelocityDamage
@@ -27,13 +21,17 @@ public class VelocityDamage
         MinecraftForge.EVENT_BUS.register(VelocityDamage.class);
     }
 
-    @SubscribeEvent
+    // Lowest priority so other mods have a chance to change the damage prior to this
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onLivingHurt(LivingHurtEvent event) {
         LOGGER.debug("Attack pre-change: " + event.getAmount());
         if (!(event.getSource().getEntity() instanceof ServerPlayer player)) return;
-        if (!(player.getMainHandItem().getItem() instanceof SwordItem)) return;
-        // 12 seconds
-        event.setAmount(event.getAmount() * 400_000F);
-        LOGGER.debug("Attack post-change: " + event.getAmount() * 400_000F);
+        if (!(player.getMainHandItem().getItem() instanceof FishingRodItem)) return;
+
+        double dotProduct = player.getDeltaMovement().dot(event.getEntity().getDeltaMovement());
+
+        // TODO: find more elegant way to calculate damage
+        event.setAmount((float) (event.getAmount() + (1 * -dotProduct)));
+        LOGGER.debug("Attack post-change: " + ((float) (event.getAmount() + (1 * -dotProduct))));
     }
 }
