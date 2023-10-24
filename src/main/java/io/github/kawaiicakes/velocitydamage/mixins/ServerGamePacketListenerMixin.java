33ef2d5a729c71f13799734946c6ecfe22c971fd
kawaiicakes <vomitmopper@gmail.com>
2023-10-24 08:13:50 -0600

    Seeded mixins into build to try and solve the player problem

---
+++ src/main/java/io/github/kawaiicakes/velocitydamage/mixins/ServerGamePacketListenerMixin.java
@@ -0,0 +1,15 @@
+package io.github.kawaiicakes.velocitydamage.mixins;
+
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public class ServerGamePacketListenerMixin {
+
+    @Inject(method="tick", at=@At("HEAD"))
+    public void tick(CallbackInfo ci) {
+    }
+}
