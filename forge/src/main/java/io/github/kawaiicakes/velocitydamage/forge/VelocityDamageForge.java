eb69282348168a5a6638bc779028d3b77fc6cc43
kawaiicakes <vomitmopper@gmail.com>
2024-03-05 13:09:58 -0700

    this should have been pushed w/ the previous commit. smh

--- forge/src/main/java/io/github/kawaiicakes/velocitydamage/forge/VelocityDamageForge.java
+++ forge/src/main/java/io/github/kawaiicakes/velocitydamage/forge/VelocityDamageForge.java
@@ -1,14 +1,11 @@
 package io.github.kawaiicakes.velocitydamage.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import io.github.kawaiicakes.velocitydamage.VelocityDamage;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(VelocityDamage.MOD_ID)
 public class VelocityDamageForge {
     public VelocityDamageForge() {
-        EventBuses.registerModEventBus(VelocityDamage.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         VelocityDamage.init();
     }
 }
