d056273adecff24ecbd66409622ab3c7a814c5d0
kawaiicakes <vomitmopper@gmail.com>
2024-03-22 01:48:08 -0600

    now injecting into #tick to hopefully catch all times $deltaPosition is changed. Also fixed volume of collision sound.

--- common/src/main/java/io/github/kawaiicakes/velocitydamage/mixins/LivingEntityMixin.java
+++ common/src/main/java/io/github/kawaiicakes/velocitydamage/mixins/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package io.github.kawaiicakes.velocitydamage.mixins;
 
 import io.github.kawaiicakes.velocitydamage.config.ConfigValues;
+import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -28,24 +29,20 @@ public abstract class LivingEntityMixin extends Entity {
         super(entityType, level);
     }
 
-    @Inject(method = "travel", at = @At(value = "HEAD"))
-    private void setMovementDelta(Vec3 travelVector, CallbackInfo ci) {
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;tick()V"))
+    private void updateMovementDelta(CallbackInfo ci) {
         this.velocitydamage$movementDeltaPseudoLocal = this.getDeltaMovement();
     }
 
-    @Inject(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;move(Lnet/minecraft/world/entity/MoverType;Lnet/minecraft/world/phys/Vec3;)V", shift = At.Shift.AFTER))
-    private void updateMovementDelta(Vec3 travelVector, CallbackInfo ci) {
-        this.velocitydamage$movementDeltaPseudoLocal = this.getDeltaMovement();
-    }
-
-    @Inject(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;calculateEntityAnimation(Lnet/minecraft/world/entity/LivingEntity;Z)V"))
-    private void doHorizontalCollisionDamage(Vec3 travelVector, CallbackInfo ci) {
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;aiStep()V", shift = At.Shift.AFTER))
+    private void checkCollisionDamage(CallbackInfo ci) {
         if (ConfigValues.getInstance().damageOnAcceleration) {
             // FIXME: damage calculation is missing a lot of the locals necessary for accurate values.
+            // FIXME: collision damage w/ elytra may still be doubled up here
             // this might actually be fine if the local float damage is doing what I think it is. I could just tweak some values around and then implement functionality for ServerPlayers
             float damage = (float) (((this.velocitydamage$movementDeltaPseudoLocal.horizontalDistance() - this.getDeltaMovement().horizontalDistance()) * 10.0F) - 3.0F);
             if (!this.isFallFlying() && this.horizontalCollision && !this.level.isClientSide && damage > 0.0F) {
-                this.playSound(PLAYER_ATTACK_CRIT, Math.min(0.2F, damage), 0.7f);
+                this.playSound(PLAYER_ATTACK_CRIT, Mth.clamp(damage, 1.0F, 5.0F), 0.7f);
                 this.hurt(DamageSource.FLY_INTO_WALL, damage);
             }
         }
